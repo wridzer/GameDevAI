48b01cc3d4a1168ca2e21119f068e1cf8c0177d1
vmuijrers <vmuijrers@gmail.com>
2021-11-23 13:45:59 +0100

    Update BTSequence.cs

--- BehaviourTreeExample/Assets/Scripts/Example/BTSequence.cs
+++ BehaviourTreeExample/Assets/Scripts/Example/BTSequence.cs
@@ -1,109 +1,132 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace BTExample
 {
-    public enum BTNodeStatus { Success, Running, Failed }
-
-    public class BTSequence : BTBaseNode
+    public abstract class BTNode
     {
-        private int currentIndex = 0;
-        private BTBaseNode[] nodes;
-        public BTSequence(params BTBaseNode[] inputNodes)
-        {
-            nodes = inputNodes;
-        }
-
-        public override BTNodeStatus Run()
-        {
-            for(; currentIndex < nodes.Length ; currentIndex++){
-            {
-                BTNodeStatus result = nodes[currentIndex].OnUpdate();
-                switch (result)
-                {
-                    case BTNodeStatus.Failed: currentIndex = 0; return BTNodeStatus.Failed;
-                    case BTNodeStatus.Success: break;
-                    case BTNodeStatus.Running: return BTNodeStatus.Running;
-                }
-            }
-            currentIndex = 0;
-            return BTNodeStatus.Success;
-        }
-    }
-
-    public class BTMove : BTBaseNode
-    {
-        private VariableFloat moveSpeed;
-        public BTMove(VariableFloat moveSpeed)
-        {
-            this.moveSpeed = moveSpeed;
-        }
-
-        public override BTNodeStatus Run()
-        {
-            moveSpeed.Value = 3;
-            return BTNodeStatus.Success;
-        }
-    }
-    public class BTAnimate : BTBaseNode
-    {
-        public BTAnimate() { }
-
-        public override BTNodeStatus Run()
-        {
-            return BTNodeStatus.Success;
-        }
-    }
-
-    public abstract class BTBaseNode
-    {
-        public virtual void OnEnter(){}
-        public virtual void OnExit(){}
-        public abstract BTNodeStatus Run();
+        public enum BTResult { Success, Failed, Running }
+        public abstract BTResult Run();
         private bool isInitialized = false;
-        public void BTNodeStatus OnUpdate(){
-            if(!isInitialized){
-                isInitialized = true;
+
+        public BTResult OnUpdate()
+        {
+            if (!isInitialized)
+            {
                 OnEnter();
+                isInitialized = true;
             }
-            var result = Run();
-            if(result != BTNodeStatus.Running){
+            BTResult result = Run();
+            if(result != BTResult.Running)
+            {
                 OnExit();
                 isInitialized = false;
             }
             return result;
         }
+
+        public virtual void OnEnter() { }
+        public virtual void OnExit() { }
     }
 
-    public class SomeAI : MonoBehaviour
+    //Composite node
+    public class BTSequence : BTNode
     {
+        private BTNode[] children;
+        private int currentIndex = 0;
 
-        [SerializeField] private VariableFloat moveSpeed;
-
-        private BTBaseNode behaviourTree;
-        private void Start()
+        public BTSequence(params BTNode[] _children)
         {
-            moveSpeed = Instantiate(moveSpeed);
-
-
-            behaviourTree = 
-                new BTSequence(
-                    new BTAnimate(),
-                    new BTMove(moveSpeed),
-                    new BTAnimate(),
-                    new BTSequence(
-                        new BTAnimate(),
-                        new BTMove(moveSpeed),
-                        new BTAnimate()
-                    )
-                );
-        }
-        
-        public void Update(){
-        
-            behaviourTree?.OnUpdate();
+            children = _children;
         }
 
+        public override BTResult Run()
+        {
+            for (; currentIndex < children.Length; currentIndex++)
+            {
+                BTResult result = children[currentIndex].OnUpdate();
+                switch (result)
+                {
+                    case BTResult.Failed:
+                        currentIndex = 0;
+                        return BTResult.Failed;
+                    case BTResult.Running:
+                        return BTResult.Running;
+                    case BTResult.Success: break;
+                }
+            }
+            currentIndex = 0;
+            return BTResult.Success;
+        }
+    }
+
+    //Composite node
+    public class BTSelector : BTNode
+    {
+        private BTNode[] children;
+        private int currentIndex = 0;
+
+        public BTSelector(params BTNode[] _children)
+        {
+            children = _children;
+        }
+
+        public override BTResult Run()
+        {
+            for (; currentIndex < children.Length; currentIndex++)
+            {
+                BTResult result = children[currentIndex].OnUpdate();
+                switch (result)
+                {
+                    case BTResult.Failed: break;
+                    case BTResult.Running:
+                        return BTResult.Running;
+                    case BTResult.Success:
+                        currentIndex = 0;
+                        return BTResult.Success;
+                }
+            }
+            currentIndex = 0;
+            return BTResult.Failed;
+        }
+    }
+
+    //Action node
+    public class BTWait : BTNode
+    {
+        private float waitTime;
+        private float currentTime;
+        public BTWait(float _waitTime)
+        {
+            waitTime = _waitTime;
+        }
+
+        public override BTResult Run()
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= waitTime)
+            {
+                currentTime = 0;
+                return BTResult.Success;
+            }
+            return BTResult.Running;
+
+        }
+    }
+
+    //Action node
+    public class BTDebug : BTNode
+    {
+        private string debugMessage;
+        public BTDebug(string _debugMessage)
+        {
+            debugMessage = _debugMessage;
+        }
+
+        public override BTResult Run()
+        {
+            Debug.Log(debugMessage);
+            return BTResult.Success;
+        }
     }
 }
+
